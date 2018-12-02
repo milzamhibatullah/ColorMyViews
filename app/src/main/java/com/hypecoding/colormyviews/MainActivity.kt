8a21e797d787d5df07e6d67aeb3c333d890a4094
Milzam Hibatullah <hmilzam@gmail.com>
2018-12-02 17:36:06 +0700

    adding 5 boxes of text and also click handling

--- app/src/main/java/com/hypecoding/colormyviews/MainActivity.kt
+++ app/src/main/java/com/hypecoding/colormyviews/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.hypecoding.colormyviews
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListener();
+    }
+
+    private fun setListener() {
+        val clicakbleViews  :   List<View>  =
+            listOf(box_one_text,box_to_text,box_three_text,box_four_text,box_five_text,
+                constraint_layout)
+
+        for (item in clicakbleViews){
+            item.setOnClickListener {
+                makeColored(it)
+            }
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_to_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
